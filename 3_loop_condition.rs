afd9dfb0a1694bbba3aab7cfdb72a349475b0764
Anurag Gupta <anurag6967@gmail.com>
2024-11-18 00:33:05 +0530

    rust variables tuble array loop condition

---
+++ 3_loop_condition.rs
@@ -0,0 +1,38 @@
+fn main() {
+    //condition
+    
+    //if-else 
+    let is_even: bool = false;
+
+    if is_even {
+        println!("if condition in rust")
+
+    }else if !is_even {
+        println!("if - else part")
+    }else {
+        println!("else part")
+    }
+
+    //loop for loop
+    
+    //it print from 0 to 10 in this loop 10 is includes
+    for i in 0..=10{
+        print!("{}",i)
+    }
+    //it print from 0 to 9 in this loop 10 is excluded(not include)
+    for i in 0..10{
+        println!("{}",i)
+    }
+
+    let a:i32=20;
+
+    for a in 0..=a {
+        println!("{}",a)
+
+    }
+
+    for i in 0..=10{
+        println!("anurag={}",i)
+    }
+
+}
